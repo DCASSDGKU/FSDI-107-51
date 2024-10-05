5fb65713582195673d865b0535921b674d5a179d
DCASSDGKU <dbc_bun@yahoo.com>
2024-10-05 11:57:30 -0700

    Add files via upload

---
+++ intro.py
@@ -0,0 +1,62 @@
+print("hello world")
+# console.log ("heelo world");
+
+# variables
+name = "dennis"
+age = 38
+found = False
+name = True
+name = 12
+# let name =  "dennis";
+# let 
+
+print(name)
+
+# if/else statement
+if age<100:
+    print("dont worry youre still young")
+elif age==100:
+    print("wow youre a century")
+else:
+    print("well you seems a bit old")
+
+# function say-Hello(){}
+def say_Hello():
+    print("hello there")
+# # function say_hello(){}
+#call functions
+say_Hello
+
+#print("my age is "+age+" years old")
+
+#array - - list
+colors = ["yellow, green, blue"]
+print(colors)
+
+#add
+colors.append("pink")
+print(colors)
+#remove using the name
+colors.remove("yellow")
+print(colors)
+#remove using the index
+colors.pop(0)
+print(colors)
+
+print(colors[1])
+
+#for loops
+#for x in colors:
+#    print(x)
+#for(let i=0;i,colors.length;i++){
+#let temp = i[0];
+#console.log(temp)
+#}
+
+#dictionary
+me = {
+    "first name":"Dencio",
+    "last_name":"chancho",
+    "age":38
+}
+print(me)
